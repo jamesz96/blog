41b8e9eefec5baee422bcfcaf3e87477e94e67d4
James Z <jamesz96.au@gmail.com>
2023-03-26 14:09:32 +1100

    Update site data (#20)

--- src/lib/data/site.ts
+++ src/lib/data/site.ts
@@ -1,6 +1,6 @@
 export default {
-  siteName: 'TODO',
-  siteUrl: 'TODO',
+  siteName: 'zhang jing',
+  siteUrl: 'zhangjing.dev',
   github: 'https://github.com/jamesz96',
   twitter: 'TODO',
   email: 'jamesz96.au@gmail.com',
