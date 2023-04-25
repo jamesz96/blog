842b576237052f80f166cc18d712e1b477f436ec
James Z <jamesz96.au@gmail.com>
2023-04-25 11:23:58 +1000

    fix: update github handle (#23)

--- src/lib/data/site.ts
+++ src/lib/data/site.ts
@@ -1,7 +1,7 @@
 export default {
   siteName: 'zhang jing',
   siteUrl: 'zhangjing.dev',
-  github: 'https://github.com/jamesz96',
+  github: 'https://github.com/jamszh',
   twitter: 'TODO',
   email: 'jamesz96.au@gmail.com',
   desc: 'All things me',
