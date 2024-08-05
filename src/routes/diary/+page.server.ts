5fae8e8db08d6ecdb1cd99f2ad3fd1da43173a79
jamszh <jamesz96.au@gmail.com>
2024-08-06 08:36:52 +1000

    fix: remove diary

--- src/routes/diary/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import type { PageServerLoad } from './$types';
-
-export const load = (async ({ fetch }) => {
-  const response = await fetch('/api/diary')
-  const { body } = await response.json()
-  return {
-    diaryEntries: body
-  }
-}) satisfies PageServerLoad
